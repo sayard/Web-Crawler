7208f8774957ec270cc1e43e8f08154843080639
Karol Gracz <graczu42@gmail.com>
2017-05-12 17:24:40 +0200

    created projects

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by Karol on 12.05.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
